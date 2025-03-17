2aed112ac3ce8e249aeb449fc8b5285bc3d19dd0
sunny <sunilswarna7@gmail.com>
2025-03-17 12:15:13 -0500

    adding datatypes

---
+++ datatypes/strings/cardno.py
@@ -0,0 +1,5 @@
+card_no=(input('enter the number'))
+lastdig = card_no[15:]
+four= '*'* 4 +' '
+display =four * 3 + lastdig
+print(display)
---
+++ datatypes/strings/sort.py
@@ -0,0 +1,3 @@
+str ='joihuivnsjkfn'
+ss=sorted(str)
+print(ss)
